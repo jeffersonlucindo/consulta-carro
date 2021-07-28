b1111b7d935cd96ac4cb71db926e37b132d68c9e
jeffersonlucindo <jeffersonlucindo@gmail.com>
2021-07-28 19:04:20 -0300

    feature: Inclsuão do projeto api-consuta-carro

---
+++ api-consulta-carro/src/main/java/br/com/carro/ApiConsultaCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.carro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsultaCarrosApplication.class, args);
+	}
+
+}
---
+++ api-consulta-carro/src/test/java/br/com/consultacarros/apiconsultacarros/ApiConsultaCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultacarros.apiconsultacarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
